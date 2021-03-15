e821dc9273d7bf03c8de8563ae10b099aea9e535
Kevin <tkevinkiragu@gmail.com>
2021-03-15 12:50:03 +0300

    revised code -v

--- index.js
+++ index.js
@@ -2,55 +2,54 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+// fetching data through fs and parsing it
 const laptopData = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`, "utf-8"));
 
 const server = http.createServer((req, res) => {
 	const pathName = url.parse(req.url, true).pathname;
 	const id = url.parse(req.url, true).query.id;
 
-	// laptop overview
 	if (pathName === "/" || pathName === "/products") {
 		res.writeHead(200, { "content-type": "text/html" }, "utf-8");
 
 		fs.readFile(`${__dirname}/templates/template-overview.html`, "utf-8", (err, data) => {
-			let overviewOutput = data;
+			let productPageOutput = data;
+
 			fs.readFile(`${__dirname}/templates/template-card.html`, "utf-8", (err, data) => {
+				// map the laptop data while replacing the cards template strings with data
 				const cardOutput = laptopData.map((el) => replaceTemplate(data, el)).join("");
-				overviewOutput = overviewOutput.replace("{%CARDS%}", cardOutput);
-				res.end(overviewOutput);
+
+				// parse the card output into the productpageoutput
+				productPageOutput = productPageOutput.replace("{%CARDS%}", cardOutput);
+
+				// send the complete rendered overview product page output
+				res.end(productPageOutput);
 			});
 		});
-	}
-
-	// laptop detail
-	else if (pathName === "/laptop" && id < laptopData.length) {
-		res.writeHead(200, { "content-type": "text/html" }, "utf-8");
-
+	} else if (pathName === "/laptop" && id < laptopData.length) {
+		// access the template laptop file
 		fs.readFile(`${__dirname}/templates/template-laptop.html`, "utf-8", (err, data) => {
+			// Get laptop data based on url query id
 			const laptop = laptopData[id];
-			const output = replaceTemplate(data, laptop);
 
-			res.end(output);
+			// parse data based on laptop data
+			const laptopOutput = replaceTemplate(data, laptop);
+			res.end(laptopOutput);
 		});
-	}
-
-	//images
-	else if (/\.(jpg|jpeg|png|gif)$/i.test(pathName)) {
+	} else if (/\.(jpg|jpeg|png|gif)$/i.test(pathName)) {
+		console.log(pathName);
 		fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
 			res.writeHead(200, { "content-type": "img/jpg" });
 			res.end(data);
 		});
-	}
-
-	// URL NOT FOUND
-	else {
+	} else {
 		res.writeHead(404, { "content-type": "text/html" }, "utf-8");
-		res.end("An error occured while accessing the page");
+		res.end("An error occured while trying access this page");
 	}
 });
 
 server.listen(1337, "127.0.0.1", () => {
-	console.log("listening for request");
+	console.log("Listening");
 });
 
 function replaceTemplate(originalHtml, laptop) {
